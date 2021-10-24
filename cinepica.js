f3348aae47175274270fdec1696266fae35eca88
cinepica <65553967+cinepica@users.noreply.github.com>
2021-10-24 15:49:08 -0300

    Update cinepica.js

--- cinepica.js
+++ cinepica.js
@@ -1319,3 +1319,32 @@ if (page == "all-films.html") {
     }
 });
 }
+
+
+// ACÁ EMPIEZA EL POP-UP
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+
+window.onload = (event) => {
+    setTimeout(function(){ modal.classList.toggle("modal-on"); }, 1000);
+  }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// ACÁ TERMINA EL POP-UP
